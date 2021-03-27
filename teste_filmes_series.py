4662572c56064fff0a279912c679b94262adb597
Joao Vicente <joao@upsites.digital>
2021-03-27 16:57:19 -0300

    Aprendendo sobre polimorfimso e aplicando o conceito guardando diferentes objetos com mesma herança em uma lista e depois imprindo a partir de um for

--- teste_filmes_series.py
+++ teste_filmes_series.py
@@ -1,13 +1,20 @@
 from model import Filme, Serie
 
-filme = Filme('vingadores - guerra infinita', 2018, 160)
-filme.dar_like()
-print(f'Nome: {filme.nome} - Ano: {filme.ano} '
-      f'Duração: {filme.duracao} - Likes: {filme.likes}')
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
+      f'Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
-serie = Serie('nao sei', 2018, 2)
-serie.nome = 'atlanta - um teste'
-serie.dar_like()
-serie.dar_like()
-print(f'Nome: {serie.nome} - Ano: {serie.ano} '
-      f'Duração: {serie.temporadas} - Likes: {serie.likes}')
+atltanta = Serie('nao sei', 2018, 2)
+atltanta.nome = 'atlanta - um teste'
+atltanta.dar_like()
+atltanta.dar_like()
+print(f'Nome: {atltanta.nome} - Ano: {atltanta.ano} '
+      f'Duração: {atltanta.temporadas} - Likes: {atltanta.likes}')
+
+#Aula do modulo 3, aula 2, polimorfismo
+
+filmes_e_series = [vingadores, atltanta]
+for programa in filmes_e_series:
+      detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+      print(f'{programa.nome} - {detalhes} D - {programa.likes}')
