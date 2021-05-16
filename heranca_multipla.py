ff6d7122a8c390a9ba92d9202ba6d1b6caeba805
Joao Vicente <joao@upsites.digital>
2021-05-16 12:04:55 -0300

    Aprendendo sobre herança multipla, como uma subclasse pode herdar atributos de duas classes mãe distintas

---
+++ heranca_multipla.py
@@ -0,0 +1,35 @@
+class Funcionario:
+    def registra_horas(self, horas):
+        print('Horas registradas...')
+
+    def mostrar_tarefas(self):
+        print('Fez muita coisa...')
+
+class Caelum(Funcionario):
+    def mostrar_tarefas(self):
+        print('Fez muita coisa, Caelumer!')
+
+    def busca_cursos_do_mes(self, mes=None):
+        print(f'Mostrando Cursos - {mes}' if mes else 'Mostrando cursos desse mês')
+
+class Alura(Funcionario):
+    def mostrar_tarefas(self):
+        print('Fez muita coisa, Alurete!')
+
+    def busca_perguntas_sem_resposta(self):
+        print('Mostrando perguntas não respondidas no fórum')
+
+class Junior(Alura):
+    pass
+
+class Pleno(Alura, Caelum):
+    pass
+
+jose = Junior()
+jose.busca_perguntas_sem_resposta()
+# jose.busca_cursos_do_mes() <- Erro, pois o objeto Jose não tem acesso ao atributo da Classe, apenas o Pleno
+
+luan = Pleno()
+luan.busca_perguntas_sem_resposta()
+luan.busca_cursos_do_mes()
+luan.mostrar_tarefas()
